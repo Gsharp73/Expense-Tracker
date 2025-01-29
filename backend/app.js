bc201a134b803db8ed3b1568bb671093d43e6d14
Gsharp73 <tripathidhyanendra2004@gmail.com>
2025-01-29 18:06:39 +0530

    cors

--- backend/app.js
+++ backend/app.js
@@ -45,7 +45,7 @@ async function startServer() {
         app.use(express.json());
         app.use(
             cors({
-                origin: process.env.FRONT_END_BASE,
+                origin: "https://expense-tracker-i4m2.vercel.app",
                 // origin: "http://localhost:5173",
                 credentials: true,
                 methods: ["GET", "POST", "PUT", "DELETE"],
