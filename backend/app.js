134a973b0f02d04c23687e8c8163396a66af0515
Gsharp73 <tripathidhyanendra2004@gmail.com>
2025-01-29 14:22:51 +0530

    k

--- backend/app.js
+++ backend/app.js
@@ -42,7 +42,7 @@ async function startServer() {
         app.use(express.json());
         app.use(
             cors({
-                origin: "*",
+                origin: "http://localhost:5173",
                 credentials: true,
                 methods: ["GET", "POST", "PUT", "DELETE"],
             })
